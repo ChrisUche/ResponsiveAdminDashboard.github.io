66b360660800efe0ed86deb725f5705aa74f2005
ChrisUche <101889570+ChrisUche@users.noreply.github.com>
2022-10-09 00:41:35 +0100

    Add files via upload
    initial commit

---
+++ orders.js
@@ -0,0 +1,40 @@
+const Orders = [
+    {
+      productName: 'Drone',
+      productNumber: '534454',
+      paymentStatus: 'Due',
+      shipping:'Pending',
+    },
+    {
+      productName: 'Fried Yam',
+      productNumber: '898953',
+      paymentStatus: 'Paid',
+      shipping:'Pending',
+    },
+    {
+      productName: 'Tesla X',
+      productNumber: '136378',
+      paymentStatus:'Due',
+      shipping: 'Declined',
+    },
+    {
+      productName: 'Burna Merch',
+      productNumber: '723001',
+      paymentStatus: 'Paid',
+      shipping:'Delivered',
+    },
+    {
+      productName: 'Vlog Stick',
+      productNumber: '355924',
+      paymentStatus: 'Paid',
+      shipping: 'Pending',
+    },
+    {
+      productName: 'iMac',
+      productNumber: '002337',
+      paymentStatus:'Due',
+      shipping:'Delivered',
+    }
+   
+   ]
+   
